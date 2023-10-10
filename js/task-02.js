b64b0e61614476ea0f574a1a66db3bada9dcd350
Aleksey-Dr <aprel181818@gmail.com>
2023-10-10 22:08:18 +0300

    Solution of task#2
    Solution of task#2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+
+const items = ingredients.map(ingredient => {
+
+  const item = document.createElement('li');
+
+  item.textContent = ingredient;
+  item.classList.add('item');
+  
+  return item;
+});
+
+listIngredients.append(...items);
+
+
+
+// =============== #2 ================ //
+// ingredients.forEach(ingredient => {
+
+//   const item = document.createElement('li');
+//   // console.log(item);
+
+//   item.textContent = ingredient;
+//   listIngredients.append(item);
+// });
