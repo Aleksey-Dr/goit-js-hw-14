402b16b3f72d79f2990d2f83a05bcfa568a046de
Aleksey-Dr <aprel181818@gmail.com>
2023-10-13 10:07:00 +0300

    Update task-01.js
    Update task-01.js
    Added properties for navigation by DOM.

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,32 @@
 const categories = document.querySelector('#categories');
-// console.log(categories);
-const items = categories.querySelectorAll('.item');
 
-console.log('Number of categories: ' + items.length);
+const items = categories.children.length;
+
+console.log('Number of categories: ' + items);
 console.log();
 
-items.forEach(item => {
-    const list = item.querySelector('ul');
-    
-    console.log('Category: ' + item.querySelector('h2').textContent);
-    console.log('Elements: ' + list.querySelectorAll('li').length);
+[...categories.children].forEach(item => {
+    console.log('Category: ' + item.firstElementChild.textContent);
+    console.log('Elements: ' + item.lastElementChild.children.length);
     console.log();
 });
+
+
+
+
+
+
+
+
+// const items = categories.querySelectorAll('.item');
+
+// console.log('Number of categories: ' + items.length);
+// console.log();
+
+// items.forEach(item => {
+//     const list = item.querySelector('ul');
+    
+//     console.log('Category: ' + item.querySelector('h2').textContent);
+//     console.log('Elements: ' + list.querySelectorAll('li').length);
+//     console.log();
+// });
